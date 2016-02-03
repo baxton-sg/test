a270ae5664ecad606da86420d8b8d15f70ec4f85
Maxim Alekseykin <baxton-sg@users.noreply.github.com>
2016-02-03 21:46:25 +0800

    mmsystole.py

--- test_lr.cpp
+++ test_lr.cpp
@@ -1,218 +1,257 @@
 
 
+
 import os
 import sys
+import csv
+import numpy as np
+import scipy.misc as spm
 from pathes import *
-from utils_wr import *
 from core import *
+from utils_wr import UTILS
 from ann import ANN
 
+import matplotlib.pyplot as plt
 
-FRAMES = 100
-ROWS = 28
-COLS = 28
-F = 2
-S = 2
-
-EPOCHES = 1000
-A=.008
-
-chanks_num = 20
+from sklearn.ensemble import RandomForestRegressor
 
 
-def max_pool(buffer, F, S):
-
-    rows = (ROWS - F) / S + 1
-    cols = (COLS - F) / S + 1
-
-    res = np.zeros((FRAMES, rows, cols), dtype=np.float64)
-
-    brow = 0
-    for r in range(rows):
-        bcol = 0
-        for c in range(cols):
-            for d in range(FRAMES):
-                p = buffer[d, brow : brow+F, bcol : bcol+F].max()
-                res[d,r,c] = p
-            bcol += S
-        brow += S
-
-    return res
+utils = UTILS()
 
 
 
-
-
-def prepare_data(F, S, meta, case_scan_id_map, systole, diastole):
- 
-    new_rows = (ROWS - F) / S + 1
-#    new_rows = ROWS
+def get_N(base_path):
     N = 0
-    for case in [d for d in os.listdir(PATH_TRAIN_F) if not d.startswith('.')]:
-        path = os.path.join(PATH_TRAIN_F, case, "study")
+    for case in [d for d in os.listdir(base_path) if not d.startswith('.')]:
+        path = os.path.join(base_path, case, "study")
         for scan in [d for d in os.listdir(path) if not d.startswith('.')]:
             N += 1
+    return N
 
+
+
+def get_minimax():
+    minimax = {}
+    with open(os.path.join(PATH_DATA, "minimax.csv"), "r") as fin:   
+        reader = csv.reader(fin, delimiter=",")
+        for vec in reader:
+            ID = int(vec[0])
+            scan = vec[1]
+            minimax[(ID, scan)] = (vec[2], vec[3])
+    return minimax
+
+
+
+
+def get_data(systole, diastole, minimax, base_path, meta, case_scan_id_map):
+    keys = minimax.keys()
+    N = len(keys)
+    to_train = int(N * .8)
     indices = range(N)
     np.random.shuffle(indices)
-    N = int(N * 1.)
-    indices = indices[:N]
-    to_train = int(N * .8)
-    train_indices = set(indices[:to_train])
-    indices = set(indices)
+    indices_train = set(indices[:to_train])
 
-    print "num:", N, to_train
 
-    idx       = 0
-    idx_train = 0
-    idx_test  = 0
+    SHIFT = 10
 
-    columns = FRAMES * new_rows * new_rows + 20
-    #columns = 20
 
-    t1 = to_train / chanks_num
-    t4 = to_train - t1 * (chanks_num - 1)
+    F = 4
+    S = 2
 
-    data = [None] * chanks_num
-    for ch in range(chanks_num):
-        data[ch] = np.zeros((t1, columns), dtype=np.float64)
-    data[-1] = np.zeros((t4, columns), dtype=np.float64)
 
-    counts = [t1] * chanks_num
-    counts[-1] = t4
-    rows_num = [0] * chanks_num
-    
-    data_test = np.zeros((N - to_train, columns), dtype=np.float64)
+    ROWS = 300
+    COLS = 300
 
-    YStrain = np.zeros((to_train,), dtype=np.float64)
-    YStest = np.zeros((N-to_train,), dtype=np.float64)
+    R = 60
+    C = 60
 
-    YDtrain = np.zeros((to_train,), dtype=np.float64)
-    YDtest = np.zeros((N-to_train,), dtype=np.float64)
 
+    FLIP_NUM = 1
+
+    META_SIZE = 0
+    meta_indices = [4,5,6,7]
+
+    data = np.zeros((to_train * FLIP_NUM, R * C + META_SIZE), dtype=np.float64)
+    data_test = np.zeros((N - to_train, R * C + META_SIZE), dtype=np.float64)
+
+    YStrain = np.zeros((to_train * FLIP_NUM,), dtype=np.float64)
+    YStest = np.zeros((N - to_train,), dtype=np.float64)
+    YDtrain = np.zeros((to_train * FLIP_NUM,), dtype=np.float64)
+    YDtest = np.zeros((N - to_train,), dtype=np.float64)
 
     idx = 0
-    for case in [d for d in os.listdir(PATH_TRAIN_F) if not d.startswith('.')]:  #[:20]:
-        ID = int(case)
-        path = os.path.join(PATH_TRAIN_F, case, "study")
-        for scan in [d for d in os.listdir(path) if not d.startswith('.')]:
-            if idx not in indices:
-                idx += 1
-                continue
+    idx_test = 0
 
-            path_full = os.path.join(path, scan)
-            fnames = [f for f in os.listdir(path_full) if not f.startswith('.')]
+    frame = np.zeros((ROWS, COLS), dtype=np.float64)
 
-            vec = np.fromfile(os.path.join(path_full, fnames[0]), dtype=np.float64, sep='')
-            vec = vec.reshape((FRAMES, ROWS, COLS))
-            vec = max_pool(vec, F, S)
 
-            if idx in train_indices:
-                chank = 0
-                while counts[chank] == 0:
-                    chank += 1
-                counts[chank] -= 1
+    ttt = 0
 
-                data[chank][rows_num[chank], :-20] = vec.reshape((columns-20,))
-                #data[chank][rows_num[chank], -20:] = meta[case_scan_id_map[(ID, scan)]].copy()
-                rows_num[chank] += 1
-                 
-          
-                YStrain[idx_train] = systole[ID-1]
-                YDtrain[idx_train] = diastole[ID-1]
-                idx_train += 1
+
+    for i in range(N):
+        key = keys[i]
+
+        orig_img = spm.imread(os.path.join(base_path, str(key[0]), "study", key[1], minimax[key][0]))
+        orig_img = orig_img.astype(np.float64)
+
+
+        frame.fill(0)
+        frame[:orig_img.shape[0], :orig_img.shape[1]] = orig_img
+        #img = utils.filter(frame, F, S, 0)
+        img = frame
+        img = utils.img_resize(img, R, C)
+
+
+        if i in indices_train:
+            if META_SIZE:
+                data[idx,:-META_SIZE] = img.flatten()
+                data[idx,-META_SIZE:] = meta[case_scan_id_map[key]][meta_indices]
             else:
-                data_test[idx_test, :-20] = vec.reshape((columns-20,))
-                #data_test[idx_test, -20:] = meta[case_scan_id_map[(ID, scan)]].copy()
-                YStest[idx_test] = systole[ID-1]
-                YDtest[idx_test] = diastole[ID-1]
-                idx_test += 1
+                if ttt < 10:
+                    ttt += 1
+                    plt.clf()
+                    plt.imshow(img)
+                    plt.show()
+                data[idx] = img.flatten()
+ 
+            YStrain[idx] = systole[key[0]]
+            YDtrain[idx] = diastole[key[0]]
             idx += 1
-    return data, data_test, YStrain, YStest, YDtrain, YDtest
+
+            if FLIP_NUM > 1:
+                if META_SIZE:
+                    data[idx,-META_SIZE:] = meta[case_scan_id_map[key]][meta_indices] 
+                else:
+                    frame.fill(0)
+                    frame[:orig_img.shape[0], :orig_img.shape[1]] = orig_img[::-1,:]
+                    img = frame
+                    img = utils.img_resize(img, R, C)
+
+                    data[idx] = img.flatten()
+#                    tmp = np.zeros(img.shape)
+#                    D = np.random.randint(4)
+#                    if D == 0:   # up
+#                        tmp[:-SHIFT,:] = img[SHIFT:,:]
+#                    elif D == 1:   # up
+#                        tmp[SHIFT:,:] = img[:-SHIFT,:]
+#                    elif D == 2:   # up
+#                        tmp[:,:-SHIFT] = img[:,SHIFT:]
+#                    elif D == 3:   # up
+#                        tmp[:,SHIFT:] = img[:,:-SHIFT]
+#                    data[idx] = tmp.flatten()
+
+
+                YStrain[idx] = systole[key[0]]
+                YDtrain[idx] = diastole[key[0]]
+                idx += 1
+        else:
+            if META_SIZE:
+                data_test[idx_test,-META_SIZE:] = meta[case_scan_id_map[key]][meta_indices]
+                data_test[idx_test,:-META_SIZE] = img.flatten()
+            else:
+                data_test[idx_test] = img.flatten()
+
+            YStest[idx_test] = systole[key[0]]
+            YDtest[idx_test] = diastole[key[0]]
+            idx_test += 1
+
+    if META_SIZE:
+        min = data[:,-META_SIZE:].min(axis=0)
+        max = data[:,-META_SIZE:].max(axis=0)
+        r = max - min
+        r[r==0] = 0.00001
+  
+        data[:,-META_SIZE:] /= r
+        data_test[:,-META_SIZE:] /= r
     
+    return data, data_test, YStrain, YStest, YDtrain, YDtest
+
+
+
+
+
 
 def main():
+    np.random.seed()
+
+    base_path = PATH_TRAIN_H2
+
+    N = get_N(base_path)
+    print N
+
     systole, diastole = load_train_vals()
+
+    minimax = get_minimax()
+    #print minimax
+
     meta, case_scan_id_map = load_metadata()
-    data, data_test, YStrain, YStest, YDtrain, YDtest = prepare_data(F, S, meta, case_scan_id_map, systole, diastole)
 
-    MAX_VAL = 610
-    YStrain /= MAX_VAL
-    YStest /= MAX_VAL
-    YDtrain /= MAX_VAL
-    YDtest /= MAX_VAL
+    data, data_test, YStrain, YStest, YDtrain, YDtest = get_data(systole, diastole, minimax, base_path, meta, case_scan_id_map)
 
-    print YStrain[:20]
-
-    COLUMNS = data[0].shape[1]
-
-    ann = None
-    if 2 == len(sys.argv):
-        ann, m, r = load_ann(sys.argv[1], no_mr=True)
-    else:
-        ss = [COLUMNS, 128, 4, 1]
-        ann = ANN(ss,  0.)
-
-
-    #
-    #
-    mins = []
-    for ch in range(chanks_num):
-        mins.append(data[ch].min(axis=0))
-    min = np.vstack(mins).min(axis=0)
-   
-    maxs = []
-    for ch in range(chanks_num): 
-        maxs.append(data[ch].max(axis=0))
-    max = np.vstack(maxs).max(axis=0)
-    
+    min = data.min(axis=0)
+    max = data.max(axis=0)
     r = max - min
     r[r==0] = 0.00001
 
-    for ch in range(chanks_num):
-        data[ch] -= min
-        data[ch] /= r
-
-
-    data_test -= min
+    data /= r
     data_test /= r
-    #
-    #
+
+    print data[:5,:20]
+    print YStrain[:10]
+
+    YStrain /= 610
+    YStest /= 610
 
 
-    for e in range(EPOCHES):
-        chank = np.random.randint(chanks_num)
-        ann.partial_fit(data[chank], YStrain, L=0, A=A)
-        print e, ann.cost.value
+    if "RF" != sys.argv[1]:
+        EPOCHES = 250
+        A = .008
 
+        ss = [data.shape[1], 152, 1]
+        ss2 = [data.shape[1], 132, 32, 1]
+        ann = ANN(ss, 0)
+        ann2 = ANN(ss2, 0)
 
-    cnt = 0
-    fname = os.path.join(PATH_CACHE, "cnn_LR.bin")
-    while os.path.exists(fname):
-        cnt += 1
-        fname = os.path.join(PATH_CACHE, "cnn_LR_%d.bin" % cnt) 
-    save_ann(ann, None, None, os.path.join(PATH_CACHE, "cnn_LR.bin"))
+        YStrain2 = 1. - YStrain
 
-    # test
-    cost = 0.
-    for i in range(data_test.shape[0]):
-        v = data_test[i].copy()
-        y = YStest[i]
-        if 0 == y:
-            continue
-        PP = ann.predict_proba(v)
-        P = PP[0,1]
-        print P, "vs", y, "(", P*MAX_VAL, "vs", y*MAX_VAL, ")" 
-        cost += np.abs(P - y)
-    E = cost / data_test.shape[0]
-    print "ERROR", E, E*MAX_VAL
+        for e in range(EPOCHES):
+            ann.partial_fit(data, YStrain, A=A)
+            ann2.partial_fit(data, YStrain2, A=A)
+            print e, ann.cost.value, ann2.cost.value
+
+    else:
+        ann = RandomForestRegressor(n_estimators=100)
+        ann.fit(data, YStrain)
+   
+
+    # testing
+    if "RF" != sys.argv[1]:
+        predictions = ann.predict_proba(data_test) 
+        predictions2 = ann2.predict_proba(data_test) 
+    else:
+        predictions = ann.predict(data_test)
+ 
+    E = 0.
+    
+
+    for i in range(predictions.shape[0]):
+        if "RF" != sys.argv[1]:
+            P = predictions[i,1]
+            P2 = predictions2[i,1]
+            P -= (1. - (P + P2))
+        else:
+            P = predictions[i]
+
+        print P, "vs", YStest[i], "(%f vs %f)" % (P*610, YStest[i]*610)
+
+        E += np.abs(P - YStest[i])
+    E = E/YStest.shape[0]
+    print "ERROR", E, "(%f)" % (E*610)
+    
+
 
 
 
 
 if __name__ == "__main__":
-    main()
-
-
-
-
+    main() 

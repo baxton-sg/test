e47710c0ada359fd4556cd9f85054c44bba4189c
Maxim Alekseykin <baxton-sg@users.noreply.github.com>
2016-02-03 21:44:43 +0800

    sqcut.py

--- utils.py
+++ utils.py
@@ -2,166 +2,199 @@
 
 import os
 import sys
-import ctypes
+import csv
 import numpy as np
+import scipy.misc as spm
 
-UTILS_DLL = ctypes.cdll.LoadLibrary("utils.dll")
+from dicom_wr import DICOM
+from utils_wr import UTILS
+from ann import ANN
+from pathes import *
+import core
+from core import *
 
 
-
-
-class UTILS(object):
-    def __init__(self):
-        pass
-
-
-    def get_statistics(self, buffer):
-        tmp = buffer
-        if tmp.dtype != np.float64:
-            tmp = tmp.astype(np.float64)
-
-        total_size = tmp.shape[0]
-        for s in range(1, len(tmp.shape)):
-            total_size *= tmp.shape[s]
-
-        mv = ctypes.c_double(0)
-        skew = ctypes.c_double(0)
-        var = ctypes.c_double(0)
-        kur = ctypes.c_double(0)
-
-        UTILS_DLL.get_statistics(ctypes.c_void_p(tmp.ctypes.data),
-                                 ctypes.c_int(total_size),
-                                 ctypes.c_void_p(ctypes.addressof(mv)),
-                                 ctypes.c_void_p(ctypes.addressof(skew)),
-                                 ctypes.c_void_p(ctypes.addressof(var)),
-                                 ctypes.c_void_p(ctypes.addressof(kur)))
-        return mv, skew, var, kur
+utils = UTILS()
 
 
 
-    def get_frequencies(self, frames, MEAN_MUL, LOW_VAL, HIGH_VAL):
-        rows = frames.shape[1]
-        cols = frames.shape[2]
-        frames_num = frames.shape[0]
-
-        frames = frames.flatten()
-        freq = np.zeros((rows * cols, ), dtype=np.float64)
-
-        UTILS_DLL.get_frequencies(ctypes.c_void_p(frames.ctypes.data),
-                                  ctypes.c_int(rows),
-                                  ctypes.c_int(cols),
-                                  ctypes.c_int(frames_num),
-                                  ctypes.c_void_p(freq.ctypes.data),
-                                  ctypes.c_double(MEAN_MUL),
-                                  ctypes.c_double(LOW_VAL),
-                                  ctypes.c_double(HIGH_VAL))
-        return freq.reshape((rows, cols))
+IMG_ROWS = 60
+IMG_COLS = 60
 
 
-    def filter(self, freq, F, S, K):
-        tmp = freq
-        if freq.dtype != np.float64:
-            tmp = tmp.astype(np.float64)
+COLUMNS = 24
 
-        rows = freq.shape[0]
-        cols = freq.shape[1]
-
-        new_rows = (rows - F) / S + 1
-        new_cols = (cols - F) / S + 1
-
-        if (float(rows - F) / S + 1) != new_rows:
-            raise Exception("Wrong Frame (%d) and Stride (%d) combination, (%d,%d)" % (F, S, rows, cols))
-
-        new_freq = np.zeros((new_rows, new_cols), dtype=np.float64)
-
-        UTILS_DLL.filter(ctypes.c_void_p(tmp.ctypes.data),
-                         ctypes.c_int(rows),
-                         ctypes.c_int(cols),
-                         ctypes.c_int(F),
-                         ctypes.c_int(S),
-                         ctypes.c_double(K),
-                         ctypes.c_void_p(new_freq.ctypes.data))
-        return new_freq
-
-
-    def get_rectangles(self, freq):
-        tmp = freq
-        if freq.dtype != np.float64:
-            tmp = tmp.astype(np.float64)
-
-        rows = freq.shape[0]
-        cols = freq.shape[1]
-
-        handle = ctypes.c_void_p(0)
-        rects_num = ctypes.c_int(0)
-
-        UTILS_DLL.rects_detect(ctypes.c_void_p(tmp.ctypes.data),
-                               ctypes.c_int(rows),
-                               ctypes.c_int(cols),
-                               ctypes.c_void_p(ctypes.addressof(handle)),
-                               ctypes.c_void_p(ctypes.addressof(rects_num)))
-
-        rects = []
-
-        if 0 == rects_num.value:
-            return rects
-
-        tmp = np.zeros((rects_num.value * 4, ), dtype=np.int32)
-
-        UTILS_DLL.rects_fetch(handle, 
-                              rects_num,
-                              ctypes.c_void_p(tmp.ctypes.data))
-        UTILS_DLL.rects_free(handle)
-
-        for r in range(rects_num.value):
-            rects.append((tmp[r * 4 + 0], tmp[r * 4 + 1], tmp[r * 4 + 2], tmp[r * 4 + 3], 0))
-
-        return rects
+MEAN = 0
+RANGE = 0
 
 
 
-    def img_resize(self, img, new_rows, new_cols):
-        new_img = np.zeros((new_rows, new_cols), dtype=np.float64)
-        UTILS_DLL.img_resize(ctypes.c_void_p(img.ctypes.data),
-                             ctypes.c_int(img.shape[0]),
-                             ctypes.c_int(img.shape[1]),
-                             ctypes.c_void_p(new_img.ctypes.data),
-                             ctypes.c_int(new_rows),
-                             ctypes.c_int(new_cols))
-        return new_img
+# meta parameters 
+STRIDE = 1
+
+# 0.819794341899 on full POSITIONS
+EPOCHES, MEAN_MUL, FRAME_SIZE, KOEF, LOW_VAL, HIGH_VAL, CUTOFF = (1, 1.5191511044878414, 3, 2.7982127210771974, 8.9221054386835821, 25.033605786648987, 2.6)
 
 
 
 
 
 
-def main():
-    from dicom_wr import DICOM
-    import scipy.stats as stats
 
-    fname = os.path.join("..", "data", "train", "1", "study", "sax_10", "IM-4562-0001.dcm")
-    print "processing", fname
 
-    dicom = DICOM()
-    dicom.verbose(False)
-    dicom.fromfile(fname)
-    buffer = dicom.img_buffer()
-    dicom.free()
-    dicom = None
 
-    utils = UTILS()
-    mv, skew, var, kur = utils.get_statistics(buffer)
+def process(in_dir, out_dir, regressor, case, scan, pos): 
 
-    print mv.value, skew.value, var.value, kur.value
-    print np.mean(buffer), stats.skew(buffer.flatten()), np.var(buffer), stats.kurtosis(buffer.flatten())
+    path = os.path.join(in_dir, case, "study", scan)
+    path_out = os.path.join(out_dir, case, "study", scan)
+    mkdir_and_clear(path_out)
 
-    ###
+    fnames = [f for f in os.listdir(path) if "dcm" in f and not f.startswith('.')]
+    files_num = len(fnames)
+   
+
+
+    BUFFERS = {}
+    METAS   = {}
+    ORIGS   = {}
+    KEYS    = {}
+
+    #
+    # load data
+    #
+    buffering(path, BUFFERS, ORIGS, METAS, KEYS, CUTOFF)
+
+#    print pos
+
+    if None == pos:
+
+        # 
+        # get rectangles
+        #
+        POSITIONS = {}
+        RECTANGLES = {}
+        core.process(path, POSITIONS, RECTANGLES, BUFFERS, ORIGS, KEYS, \
+                    EPOCHES, MEAN_MUL, FRAME_SIZE, STRIDE, KOEF, LOW_VAL, HIGH_VAL, remove_zeros=False)
+
+        rects = RECTANGLES[path]
+
+        #
+        # get the best rectangle
+        #
+        bestR = None
+        bestP = 0
+        data = np.zeros((1,COLUMNS), dtype=np.float64)
+        for i in range(len(rects)):
+            r = rects[i]
+    
+            data[0,:20] = METAS[path]
+            data[0,20:] = r[:4]
+
+            data = norm(data)
+
+            predictions = regressor.predict_proba(data)
+            P = predictions[0, 1]
+
+            if P > bestP:
+                bestP = P
+                bestR = rects[i]
+
+    else:
+        bestR = pos
+        bestP = 1.
+
+ 
+    #
+    # cut rects
+    #
+#    import matplotlib.pyplot as plt
+#
+#    SP = [6, 6, 1]
+#    plt.clf()
+#    plt.subplot(*SP)
+#
+#    b = BUFFERS[KEYS[path][0]]
+#    plt.imshow(b)
+#    SP[-1] += 1
+ 
+    for f in range(files_num):
+        key = KEYS[path][f]
+        dicom = DICOM()
+        dicom.verbose(False)
+        dicom.fromfile(key)
+        orig = dicom.img_buffer().astype(np.float64)
+        dicom.free()
+        b = BUFFERS[key]
+
+        print bestR, "(*)"
+
+        if None == pos:
+            VK = float(b.shape[0]) / orig.shape[0]
+            HK = float(b.shape[1]) / orig.shape[1]
+            bestP = (bestR[0] / VK, bestR[1] / VK, bestR[2] / HK, bestR[3] / HK, 0)
+        print bestR
+        b = orig
+        
+
+        print bestR, bestP
+        r = b[bestR[0] : bestR[1] + 1, bestR[2] : bestR[3] + 1].copy()
+
+        #b = utils.img_resize(r, IMG_ROWS, IMG_COLS)
+        b = r
+
+        fn = os.path.join(path_out, fnames[f]) + ".png"
+        with open(fn, "wb+") as fout:
+            spm.imsave(fn, b)
+
+#        plt.subplot(*SP)
+#        SP[-1] += 1
+#        plt.imshow(b)
+#    plt.show()
+
+            
+            
+
+def norm(data):
+    return (data - MEAN) / RANGE
+
+
+
+
+
+
+
+
+def main(): 
+    global MEAN, RANGE
+
+    # "train" or "validate" - names of dirs
+    train_validate = sys.argv[1]
+    
+    path = os.path.join(PATH_DATA, train_validate)
+
+    regressor, MEAN, RANGE = load_ann(os.path.join(PATH_CACHE, "sq_ann.bin"))
+
+    positions, indices = load_positions()
+
+    for case in [d for d in os.listdir(path) if not d.startswith('.')]:
+        path_case = os.path.join(path, case, "study")
+        for scan in [d for d in os.listdir(path_case) if not d.startswith('.')]:
+            print case, scan
+            key = os.path.join(case, "study", scan)
+            pos = None
+            if key in positions:
+                pos = positions[key]
+            process(path, path + "_h2", regressor, case, scan, pos)
+
 
 
 
 
 if __name__ == "__main__":
     main()
-     
+
+
+
+
 
 
